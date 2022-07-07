252a32af6ef787cb4fa1e7810e1655b2018d2bcb
bfiadonu <bfiadonu@gmail.com>
2022-07-07 15:40:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import ProductList from './components/ProductList';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+
+      <BrowserRouter>
+      <div>
+
+      <Navbar />
+
+    <Routes>
+      <Route exact path='/' element={<ProductList />} />
+      <Route path='/details' element={<Details />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/default' element={<Default />} />
+      
+      </Routes>
+
+      <Modal/>
+      </div>
+      </BrowserRouter>
     </div>
+         
+  
   );
 }
 
